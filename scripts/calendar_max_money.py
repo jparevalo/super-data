6ac9dd0beab723358490be55baf57aad65e5e69e
ababbott <ababbott@miuandes.cl>
2017-09-10 18:54:15 -0300

    add all the scripts

---
+++ scripts/calendar_max_money.py
@@ -0,0 +1,38 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+from mrjob.protocol import JSONValueProtocol
+import re
+import itertools
+import csv
+
+class UsersCount(MRJob):
+    def mapper_userid(self, _, line):
+        calendar_data = line.split(",")
+	# check if it's aviable
+	if calendar_data[2] != "f" and calendar_data[3] != "price":
+		price = calendar_data[3].split("$")[1]
+        	yield [calendar_data[0], float(price) ]
+
+    def reducer(self, key, values):
+        #values2 = [value for value in values if value != ""]
+	#total = float(sum(1 for _ in values)) 
+	suma = 0
+	total = 0
+	for value in values:
+		suma += value
+		total += 1
+	
+        yield ['AVG',[suma/total ,key]]
+
+    def max_reducer(self, stat, values):
+        TEMP = [values]
+        yield [stat,max(values)]
+
+    def steps(self):
+        return [MRStep(mapper=self.mapper_userid, reducer=self.reducer),
+            MRStep(reducer=self.max_reducer)]
+
+
+if __name__ == '__main__':
+    UsersCount.run()
+
