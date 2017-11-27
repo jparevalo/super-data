014ff42d0c62d998757d54f8fa8cf9c9d1361e0b
Huarn <jparevalo@miuandes.cl>
2017-11-27 13:22:35 -0300

    min/max prices

---
+++ Scripts/graph-min-listing-price.py
@@ -0,0 +1,37 @@
+import matplotlib.pyplot as plt
+import plotly.plotly as py
+import collections
+import operator
+import sys
+import csv
+
+D = {}
+prices = {"AMSTERDAM":1.17825, "DUBLIN":1.17825, "HONG KONG":0.128144, "NEW YORK":1, "MADRID":1.17825, "PARIS":1.17825} # Euro, Euro, HKD, USD, Euro, Euro
+
+text_file_path = sys.argv[1]
+txtfile = open(text_file_path, "r")
+city = ""
+price = 0
+for index, line in enumerate(txtfile):
+	if index%2 == 0:
+		city = line.strip()
+	else:
+	    price = prices[city] * float(line.split()[3])
+	D[city] = round(price,2)
+
+D = collections.OrderedDict(sorted(D.items(), key=operator.itemgetter(1)))
+
+
+dictionary = plt.figure()
+
+plt.bar(range(len(D)), D.values(), align='center')
+plt.title("Min Listing Price (Same Currency)")
+plt.xlabel("City")
+plt.ylabel("Average Price (US$)")
+x = range(len(D))
+y = D.values()
+for a,b in zip(x, y):
+    plt.text(a, b, str(b))
+plt.xticks(range(len(D)), D.keys())
+
+plt.savefig(sys.argv[2])
---
+++ Scripts/listing_min_price.py
@@ -0,0 +1,27 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+from mrjob.protocol import JSONValueProtocol
+import re
+import itertools
+import csv
+
+class UsersCount(MRJob):
+    def mapper_userid(self, _, line):
+        calendar_data = line.split(",")
+        listing_id = calendar_data[0]
+        bedrooms = calendar_data[1]
+        price = calendar_data[2]
+        reviews = calendar_data[3]
+        if price != "price" and bedrooms == "1":
+	    if float(price) > 10:
+            	yield ["A", float(price)]
+
+    def reducer(self, key, values):
+        yield ['AVG PER LISTING', min(values)]
+
+    def steps(self):
+        return [MRStep(mapper=self.mapper_userid, reducer=self.reducer)]
+
+
+if __name__ == '__main__':
+    UsersCount.run()
