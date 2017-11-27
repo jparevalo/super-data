97b2d0b053dd015b5da6b6e9adc39200f416cc12
ababbott <ababbott@miuandes.cl>
2017-11-27 13:33:24 -0300

    new data and other stuff

---
+++ Scripts/rating_listing_avg.py
@@ -0,0 +1,43 @@
+from mrjob.job import MRJob
+from mrjob.step import MRStep
+from mrjob.protocol import JSONValueProtocol
+import re
+import itertools
+import csv
+ 
+class UsersCount(MRJob):
+
+    def mapper_movie(self, _, line):
+        listing_data = line.split(",")
+	if listing_data[4] != "":
+	    try:
+		yield ["cal", float(listing_data[4])]
+	    except:
+		pass
+ 
+    def reducer(self, key, values):
+	suma = 0
+	total = 0
+	for value in values:
+	    suma += value
+	    total +=1
+	    
+	yield ["Avg1", suma / total]
+ 
+    def reducer2(self, key, values):
+	suma = 0
+	total = 0
+	for value in values:
+	    suma += value
+	    total +=1
+        yield ['Avg2', suma / total ]
+	 
+    def steps(self):
+        #return [MRStep(mapper=self.mapper_movie)]
+	return [MRStep(mapper=self.mapper_movie, reducer=self.reducer),
+		MRStep(reducer = self.reducer2)]
+
+ 
+ 
+if __name__ == '__main__':
+    UsersCount.run()
